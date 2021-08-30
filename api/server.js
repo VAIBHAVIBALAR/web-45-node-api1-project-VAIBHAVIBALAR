b40b9667b3bd5c852f34d63122faf4cb8351f128
vaibhavibalar <vaibhavi123balar@gmail.com>
2021-08-30 13:36:03 -0700

    initialtestcommit

--- api/server.js
+++ api/server.js
@@ -1,3 +1,18 @@
 // BUILD YOUR SERVER HERE
+const express = require('express')
+const users = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+server.use(express.json())
+
+server.get('/api/users',(req, res)=>{
+    users.find()
+    .then(user =>{
+        res.status(200).json(user)
+    })
+    .catch(err =>{
+        console.log(err)
+        res.status(500).json({message: err.message})
+    })
+} )
+module.exports = server; // EXPORT YOUR SERVER instead of {}
